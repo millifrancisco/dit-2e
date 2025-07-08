7474d6b6b51cfa167be656f81e348139ef2ab012
unknown <mjnogoy0707@gmail.com>
2025-07-08 13:35:56 +0800

    added view customer details

--- pages/index.vue
+++ pages/index.vue
@@ -34,8 +34,79 @@
               ></v-img>
             </v-avatar>
           </div>
+        </template> <template v-slot:item.actions="{ item }">
+          <v-btn color="Primary" @click="getCustomersDetails(item)">View</v-btn>
         </template>
-      </v-data-table>
+      </v-data-table> 
+      <v-dialog v-model="customerDialog" width="500"> 
+        <v-card>
+        <v-card-title class="text-h5 blue dark-2" justify="space-around">
+          <span class="group pa-2">
+        <v-icon small>ℹ</v-icon>
+      </span>
+          Customer Information
+        </v-card-title>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-img src=""></v-img>
+          </v-col>
+          <v-col cols="12" md="8">
+          <v-list>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-subtitle>Name</v-list-item-subtitle>
+                <v-list-item-title>{{ name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-subtitle>Email</v-list-item-subtitle>
+                <v-list-item-title>{{ name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-subtitle>Phone</v-list-item-subtitle>
+                <v-list-item-title>{{ name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-subtitle>Age</v-list-item-subtitle>
+                <v-list-item-title>{{ name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            
+          </v-list>
+          </v-col>
+        </v-row>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+      depressed
+      color="dark blue"
+    >
+      UPDATE
+    </v-btn>
+
+
+    <v-btn
+      depressed
+      color="red"
+    >
+      DELETE
+    </v-btn>
+
+        </v-card-actions>
+      </v-card>
+      </v-dialog>
+
     </v-col>
   </v-row>
 </template>
@@ -59,9 +130,11 @@ export default {
         { text: "Address", value: "address" },
         { text: "Age", value: "age" },
         { text: "Status", value: "isVip" },
+        { text: "", value: "actions" },
       ],
       customers: [],
       loading: true,
+      customerDialog: false,  
     };
   },
   methods: {
@@ -77,6 +150,12 @@ export default {
           console.log(err);
         });
     },
+
+    getCustomersDetails(item) {
+      console.log(item);
+
+      this.customerDialog=item.name
+    }
   },
 
   mounted() {
